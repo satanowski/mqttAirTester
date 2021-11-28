3239614f4d3ae56a66d4b3ae0ca3bd543642a7de
satanowski <satanowski@gmail.com>
2021-11-28 01:01:29 +0100

    Get rid of temp/humid measure

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,26 @@
 #include "SSD1306Wire.h"
-#include "SSD1306Brzo.h"
-#include "SH1106Brzo.h"
 #include "SdsDustSensor.h"
 #include <ESP8266WiFi.h>
 #include <MQTT.h>
 #include "Wire.h"
-#include "Adafruit_SHT31.h"
+#include "creds.h"
+#include "font.h"
 
-#define SHT31_ADDRESS   0x44
 
-const char ssid[] = "-";
-const char pass[] = "-";
-const char broker[] = "-.-.-";
-const int port = 1883;
-const char clientID[]= "-";
-const char mqttUser[] = "-";
-const char mqttPass[] = "-";
 const int rxPin = D5;
 const int txPin = D6;
 const int btnLeft = D3;
 const int btnRight = D7;
-const char topic0[] = "home/air/hearbeat";
-const char topic1[] = "home/air/pm10";
-const char topic2[] = "home/air/pm25";
-const char topic3[] = "home/air/temp";
-const char topic4[] = "home/air/humid";
 const int interval = 10 * 100 * 60; // 10 minutes
 
 SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
-Adafruit_SHT31 sht31 = Adafruit_SHT31();
 SdsDustSensor sds(rxPin, txPin);
 WiFiClient wifi;
 MQTTClient mqtt;
+
 long i = 0;
 float pm1 = 0.0;
 float pm2 = 0.0;
-float temp = 0.0;
-float humid = 0.0;
 
 
 void connect() {
@@ -46,14 +30,10 @@ void connect() {
     delay(1000);
   }
   Serial.println();
-  while (!mqtt.connect(clientID, mqttUser, mqttPass, false)) {
+  while (!mqtt.connect("drsSmogMiarka", mqtt_user, mqtt_pass, false)) {
     Serial.print(".");
     delay(1000);
   }
-}
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
 }
 
 void drawText(int x, int y, String s) {
@@ -76,44 +56,6 @@ void showIP() {
   display.display();
 }
 
-void setup() {
-  i = 0;
-  pinMode(btnLeft, INPUT_PULLUP);
-  pinMode(btnRight, INPUT_PULLUP);
-  Serial.begin(115200);
-  Serial.println();
-
-  display.init();
-  display.setFont(ArialMT_Plain_16);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  sds.begin();
-  sds.setQueryReportingMode();
-
-  Wire.begin();
-  Wire.setClock(100000);
-
-  if (! sht31.begin(SHT31_ADDRESS)) {
-    Serial.println("Couldn't find SHT31");
-    display.clear();
-    display.setFont(ArialMT_Plain_16);
-    drawText(0, 0, "Cannot find Temp Sensor!");
-    display.display();
-    while (1) delay(1);
-  }
-
-  splash();
-
-  WiFi.begin(ssid, pass);
-  mqtt.begin(broker, wifi);
-  mqtt.onMessage(messageReceived);
-  connect();
-
-  showIP();
-  delay(3000);
-
-  if (!mqtt.connected()) connect();
-  mqtt.publish(topic0, "esp01-alive");
-}
 
 void measInProgress() {
   display.clear();
@@ -131,14 +73,11 @@ void mqttInProgress() {
 
 
 void displayResult() {
-  Serial.println("PM25/10: " + String(pm1) + " / " + String(pm2));
-  Serial.println("T/H:" + String(temp) + " / " + String(humid));
   display.clear();
-  display.setFont(ArialMT_Plain_10);
+  display.setFont(Roboto_Mono_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  drawText(0, 0, "PM 2.5"); drawText(65, 0, String(pm2));
-  drawText(0,10, "PM 10");  drawText(65,10, String(pm1));
-  drawText(0,20, "T/H");    drawText(65,20, String(temp) + " / " + String(humid));
+  drawText(0, 0, "PM 2.5"); drawText(55, 0, String(pm2));
+  drawText(0, 20, "PM 10");  drawText(55,20, String(pm1));
   display.display();
 }
 
@@ -147,10 +86,7 @@ void mqttPublish(){
     if (!mqtt.connected()) connect();
     mqtt.loop();
     Serial.println("Publishing MQTT message...");
-    mqtt.publish(topic1, String(pm1));
-    mqtt.publish(topic2, String(pm2));
-    mqtt.publish(topic3, String(temp));
-    mqtt.publish(topic4, String(humid));
+    mqtt.publish(mqtt_topic, "{\"pm25\": " + String(pm2) + ",\"pm10\": " + String(pm1) + "}");
 }
 
 void makeMeas() {
@@ -159,11 +95,9 @@ void makeMeas() {
   measInProgress();
   delay(30000); // warm up
   PmResult pm = sds.queryPm();
-  temp = sht31.readTemperature();
-  humid = sht31.readHumidity();
   pm1 = pm.pm10;
   pm2 = pm.pm25;
-  success = pm.isOk() && !isnan(temp) && ! isnan(humid);
+  success = pm.isOk();
 
   if (success){
     mqttPublish();
@@ -182,23 +116,51 @@ void makeMeas() {
 }
 
 
+void setup() {
+  i = 0;
+  pinMode(btnLeft, INPUT_PULLUP);
+  pinMode(btnRight, INPUT_PULLUP);
+  Serial.begin(115200);
+  Serial.println();
+
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+  sds.begin();
+  sds.setQueryReportingMode();
+  Wire.begin();
+  Wire.setClock(100000);
+
+  splash();
+
+  WiFi.begin(ssid, pass);
+  mqtt.begin(mqtt_server, wifi);
+  connect();
+
+  showIP();
+  delay(2000);
+  display.clear();
+  display.display();
+  makeMeas();
+}
+
+
 void loop() {
   if (i>=interval) {
     makeMeas();
     i = 0;
   }
 
-  if (digitalRead(btnLeft)) {
+  if (!digitalRead(btnLeft)) makeMeas();
+  if (!digitalRead(btnRight)) {
+    displayResult();
+    delay(3000);
+  } else {
     display.clear();
     display.display();
-  } else {
-    displayResult();
   }
-
-  if (!digitalRead(btnRight)){
-    makeMeas();
-  }
-
   delay(100);
   i++;
 }
